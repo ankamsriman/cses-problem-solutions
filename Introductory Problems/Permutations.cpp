430937e1d0dd4dea56bd4c1e30853204597165b7
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-01-24 00:50:38 +0530

    Create Permutations.cpp

---
+++ Introductory Problems/Permutations.cpp
@@ -0,0 +1,78 @@
+//ANKAM SRIMAN
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i,a,b) for(long long int i = a; i<b; i++)
+#define fd(i,a,b) for(long long int i = a; i>=b; i--)
+#define vl(a,n) vector<ll>a(n)
+#define scarr(a) for(long long int i=0;i<a.size();i++) cin>>a[i]
+#define parr(a) for(long long int i=0;i<a.size();i++) cout<<a[i]<<" "
+#define ll long long
+#define ull unsigned long long int
+//transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout<<"\n"
+#define pr(x) cout<<x<<"\n"
+#define star cout<<"*"
+#define cy cout<<"YES\n"
+#define cn cout<<"NO\n"
+#define f first
+#define s second
+#define ii pair<ll,ll>
+
+
+const ll mod =  1e9 + 7;
+const ll N =  100000;
+
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    if(n==2||n==3)
+    {
+        cout<<"NO SOLUTION";
+    }
+    else if(n==4)
+    {
+        cout<<"3 1 4 2";
+    }
+    else
+    {
+        fi(i,1,n+1)
+        {
+            if(i&1)cout<<i<<" ";
+        }
+        fi(i,1,n+1)
+        {
+            if((i&1)==0)cout<<i<<" ";
+        }
+    }
+
+
+}
+
+signed main()
+{
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+
+    ll t=1;
+    // cin >> t; 
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0 ;
+}
