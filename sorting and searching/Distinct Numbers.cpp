a6dfccba1217da98c77d96dff95e2d368d24e8b3
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-02-14 07:21:38 +0530

    Create Distinct Numbers.cpp

---
+++ sorting and searching/Distinct Numbers.cpp
@@ -0,0 +1,71 @@
+//ANKAM SRIMAN
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i,a,b) for(long long int i = a; i<b; i++)
+#define fd(i,a,b) for(long long int i = a; i>=b; i--)
+#define vl(a,n) vector<ll>a(n)
+#define scarr(a) for(long long int i=0;i<a.size();i++) cin>>a[i]
+#define parr(a) for(long long int i=0;i<a.size();i++) cout<<a[i]<<" "
+#define ll long long
+#define ull unsigned long long int
+//transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout<<"\n"
+#define pr(x) cout<<x<<"\n"
+#define star cout<<"*"
+#define cy cout<<"YES\n"
+#define cn cout<<"NO\n"
+#define f first
+#define s second
+#define ii pair<ll,ll>
+
+
+const ll mod =  1e9 + 7;
+const ll N =  100000;
+
+
+
+void solve()
+{
+    ll n;
+    cin>>n;
+    vector<ll>a(n);
+    fi(i,0,n) cin>>a[i];
+    ll ans=1;
+    sort(a.begin(), a.end());
+    fi(i,0,n-1)
+    {
+        if(a[i]!=a[i+1])ans++;
+    }
+    cout<<ans;
+    nl;
+    
+
+
+}   
+
+signed main()
+{
+
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+
+    ll t=1;
+    // cin >> t; 
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0 ;
+}
