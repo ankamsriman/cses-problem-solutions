fc578d3b57ec8ec49e4f765ae59630b22010c89f
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2023-02-21 05:08:14 +0530

    Update Concert Tickets.cpp

--- sorting and searching/Concert Tickets.cpp
+++ sorting and searching/Concert Tickets.cpp
@@ -1,66 +1,68 @@
-//ANKAM SRIMAN
+// ANKAM SRIMAN
 #include <bits/stdc++.h>
 using namespace std;
 
-#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define IOS                      \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);
 
-#define fi(i,a,b) for(long long int i = a; i<b; i++)
-#define fd(i,a,b) for(long long int i = a; i>=b; i--)
-#define vl(a,n) vector<ll>a(n)
-#define scarr(a) for(long long int i=0;i<a.size();i++) cin>>a[i]
-#define parr(a) for(long long int i=0;i<a.size();i++) cout<<a[i]<<" "
+#define fi(i, a, b) for (long long int i = a; i < b; i++)
+#define fd(i, a, b) for (long long int i = a; i >= b; i--)
+#define vl(a, n) vector<ll> a(n)
+#define scarr(a)                                 \
+    for (long long int i = 0; i < a.size(); i++) \
+    cin >> a[i]
+#define parr(a)                                  \
+    for (long long int i = 0; i < a.size(); i++) \
+    cout << a[i] << " "
 #define ll long long
 #define ull unsigned long long int
-//transform(s.begin(), s.end(), s.begin(), ::tolower);
+// transform(s.begin(), s.end(), s.begin(), ::tolower);
 
 #define spa " "
-#define nl cout<<"\n"
-#define pr(x) cout<<x<<"\n"
-#define star cout<<"*"
-#define cy cout<<"YES\n"
-#define cn cout<<"NO\n"
+#define nl cout << "\n"
+#define pr(x) cout << x << "\n"
+#define star cout << "*"
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
 #define f first
 #define s second
-#define ii pair<ll,ll>
+#define ii pair<ll, ll>
 
-
-const ll mod =  1e9 + 7;
-const ll N =  100000;
-
-
+const ll mod = 1e9 + 7;
+const ll N = 100000;
 
 void solve()
 {
-    ll m,n;
-    cin>>n>>m;
-    vector<ll>h(n),t(m);
+    ll n, m;
+    cin >> n >> m;
+    vector<ll> h(n), t(m);
     scarr(h);
     scarr(t);
     // sort(h.begin(), h.end());
-    set<ll>s;
-    fi(i,0,h.size())s.insert(h[i]);
+    multiset<ll> s;
+    fi(i, 0, h.size()) s.insert(h[i]);
 
-    fi(i,0,t.size())
+    fi(i, 0, t.size())
     {
-        auto x=s.lower_bound(t[i]+1);
-        if(x==s.begin())
+        auto x = s.lower_bound(t[i] + 1);
+        if (x == s.begin())
         {
-            cout<<"-1\n";
+            cout << "-1\n";
         }
         else
         {
             x--;
-            cout<<(*x);
+            cout << (*x);
             s.erase(x);
             nl;
         }
     }
-
-}   
+}
 
 signed main()
 {
-
 
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -69,13 +71,13 @@ signed main()
 
     IOS;
 
-    ll t=1;
-    // cin >> t; 
+    ll t = 1;
+    // cin >> t;
     while (t--)
     {
         // q++;
 
         solve();
     }
-    return 0 ;
+    return 0;
 }
