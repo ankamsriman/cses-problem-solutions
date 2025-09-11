ab71b04dfb7e4c0cfde5e9dab4c0d7c0937daee2
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2025-09-12 05:13:29 +0530

    Update and rename Sum of Two Values.cpp to 7. Sum of Two Values.cpp

--- sorting and searching/7. Sum of Two Values.cpp
+++ sorting and searching/7. Sum of Two Values.cpp
@@ -1,23 +1,20 @@
 // ANKAM SRIMAN
+// https://cses.fi/problemset/task/1640
 #include <bits/stdc++.h>
 using namespace std;
 
-#define IOS                      \
-    ios::sync_with_stdio(false); \
-    cin.tie(NULL);               \
-    cout.tie(NULL);
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 #define fi(i, a, b) for (long long int i = a; i < b; i++)
 #define fd(i, a, b) for (long long int i = a; i >= b; i--)
 #define vl(a, n) vector<ll> a(n)
-#define scarr(a)                                 \
-    for (long long int i = 0; i < a.size(); i++) \
-    cin >> a[i]
-#define parr(a)                                  \
-    for (long long int i = 0; i < a.size(); i++) \
-    cout << a[i] << " "
+#define scarr(a) for (long long int i = 0; i < a.size(); i++) cin >> a[i]
+#define parr(a)  for (long long int i = 0; i < a.size(); i++) cout << a[i] << " "
+#define prr(a)  for (auto x:a) cout << x << " "
+#define vt(a,n) vector<long long int>a(n)
 #define ll long long
 #define ull unsigned long long int
+#define pb(x) push_back(x)
 // transform(s.begin(), s.end(), s.begin(), ::tolower);
 
 #define spa " "
@@ -28,45 +25,46 @@ using namespace std;
 #define cn cout << "NO\n"
 #define f first
 #define s second
-#define ii pair<ll, ll>
+#define ii pair<ll,ll>
 
+
+using state =pair<int,int>;
 const ll mod = 1e9 + 7;
 const ll N = 100000;
 
+
 void solve()
 {
-    ll n,s;
-    cin>>n>>s;
+    ll n,x;cin>>n>>x;
     vector<pair<ll,ll>>a;
     fi(i,0,n)
     {
-        ll x;
-        cin>>x;
-        a.push_back({x,i+1});
+        ll z;cin>>z;
+        a.push_back({z,i+1});
     }
-    sort(a.begin(), a.end());
+    sort(a.begin(),a.end());
+
+    
     fi(i,0,n)
     {
-        ll lo=i+1,hi=n-1,ans;
+        ll lo=i+1,hi=n-1,ans=-1;
         while(lo<=hi)
         {
             ll mid=(lo+hi)/2;
-            if(a[mid].f==s-a[i].f)
+            if(a[mid].first==x-a[i].first)
             {
-                cout<<a[i].s<<" "<<a[mid].s;
+                cout<<a[mid].second<<" "<<a[i].second;
                 return;
-
             }
-            else if(a[mid].f>s-a[i].f)
+            else if(a[mid].first>x-a[i].first)
             {
                 hi=mid-1;
             }
             else
             {
-                lo=mid+1;   
+                lo=mid+1;
             }
         }
-        
     }
     cout<<"IMPOSSIBLE";
     
@@ -82,9 +80,10 @@ signed main()
 #endif
 
     IOS;
-
     ll t = 1;
     // cin >> t;
+    // string x;
+    // getline(cin,x);
     while (t--)
     {
         // q++;
