5517b3ac0c381d6f350ad9ff218f84ec4106c47e
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2025-09-13 15:20:13 +0530

    Create 3. Exponentiation II.cpp

---
+++ Mathematics/3. Exponentiation II.cpp
@@ -0,0 +1,81 @@
+// ANKAM SRIMAN
+// https://cses.fi/problemset/task/1712/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define fi(i, a, b) for (long long int i = a; i < b; i++)
+#define fd(i, a, b) for (long long int i = a; i >= b; i--)
+#define vl(a, n) vector<ll> a(n)
+#define scarr(a) for (long long int i = 0; i < a.size(); i++) cin >> a[i]
+#define parr(a)  for (long long int i = 0; i < a.size(); i++) cout << a[i] << " "
+#define prr(a)  for (auto x:a) cout << x << " "
+#define vt(a,n) vector<long long int>a(n)
+#define ll long long
+#define ull unsigned long long int
+#define pb(x) push_back(x)
+// transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+#define spa " "
+#define nl cout << "\n"
+#define pr(x) cout << x << "\n"
+#define star cout << "*"
+#define cy cout << "YES\n"
+#define cn cout << "NO\n"
+#define f first
+#define s second
+#define ii pair<ll,ll>
+
+
+using state =pair<int,int>;
+const ll mod = 1e9 + 7;
+const ll N = 100000;
+
+
+ll power(ll x,ll y, ll mod)
+{
+    ll ans=1;
+    x%=mod;
+    while(y)
+    {
+        if(y&1)
+        {
+            ans=(ans*x)%mod;
+        }
+        y/=2;
+        x=(x*x)%mod;
+    }
+    return ans;
+}
+void solve()
+{
+    ll a,b,c;
+    cin>>a>>b>>c;
+    cout<<power(a,power(b,c,1e9+7-1),1e9+7)<<"\n";
+    
+
+}
+
+signed main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+    ll t = 1;
+    cin >> t;
+    // string x;
+    // getline(cin,x);
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0;
+}
