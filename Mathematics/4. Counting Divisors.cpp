d9d1371b863fcf7ba1eb0fc7042adf665a633234
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2025-09-13 16:06:07 +0530

    Update 4. Counting Divisors.cpp

--- Mathematics/4. Counting Divisors.cpp
+++ Mathematics/4. Counting Divisors.cpp
@@ -6,24 +6,54 @@ using namespace std;
 
 #define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-void solve()
+const int N=1e6;
+int sp[N+1];
+void seive()
 {
-    int n;cin>>n;
-    int ans=0;
-    for(int i=1;i*i<=n;i++)
+    for(int i=2;i<=N;i++)
     {
-        if(n%i==0)
+        sp[i]=i;
+    }
+    for(int i=2;i*i<=N;i++)
+    {
+        if(sp[i]==i)
         {
-            ans++;
-            if(i*i!=n)
+            for(int j=i*i;j<=N;j+=i)
             {
-                ans++;
+                if(sp[j]==j)
+                {
+                    sp[j]=i;
+                }
             }
-
         }
     }
-    cout<<ans<<"\n";
+}
 
+int nofdiv(int n)
+{
+    int ans=1;
+    while(n>1)
+    {   int cnt=0,p=sp[n];
+        while(n%p==0)
+        {
+            n/=p;
+            cnt++;
+        }
+        ans*=(cnt+1);
+    }
+    return ans;
+}
+void solve()
+{
+    seive();
+
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<nofdiv(n)<<"\n";
+    }
 }
 
 signed main()
@@ -36,7 +66,7 @@ signed main()
 
     IOS;
     int t = 1;
-    cin >> t;
+    // cin >> t;
     // string x;
     // getline(cin,x);
     while (t--)
