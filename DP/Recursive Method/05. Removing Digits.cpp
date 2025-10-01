5a108b26ae105357adcf076d61b2b119368e1bcf
ankamsriman <57312096+ankamsriman@users.noreply.github.com>
2025-10-02 04:16:55 +0530

    Create 05. Removing Digits.cpp

---
+++ DP/Recursive Method/05. Removing Digits.cpp
@@ -0,0 +1,78 @@
+// ANKAM SRIMAN
+// https://cses.fi/problemset/task/1637
+
+/*
+You are given an integer n. On each step, you may subtract one of the digits from the number.
+How many steps are required to make the number equal to 0?
+27 -> 20 -> 18 -> 10 -> 9 -> 0.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long int
+#define nl cout<<"\n";
+const int mod=1e9+7;
+
+// dp[n]: number of steps to make n =0
+// n=abc: dp[n]=min(dp[abc-a],dp[abc-b],dp[abc-c])
+// T=O(nlogn),Space=O(n)
+vector<ll>dp;
+
+set<int> dig(int n)
+{
+    set<int>s;
+    while(n)
+    {
+        s.insert(n%10);
+        n/=10;
+    }
+    return s;
+}
+
+ll rec(int n)
+{
+    if(n==0) return 0;
+    
+    if(dp[n]!=-1) return dp[n];
+
+    ll ans=1e18;
+    
+    for(int x:dig(n))
+    {
+        if(n-x>=0 &&x!=0)ans=min(ans,1+rec(n-x));
+    }
+    return dp[n]=ans;
+}
+
+void solve() 
+{
+    int n;cin>>n;
+    dp.resize(n+1,-1);
+
+    cout<<rec(n);
+
+}
+
+
+signed main()
+{
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    IOS;
+    int t = 1;
+    // cin >> t;
+    // string x;
+    // getline(cin,x);
+    while (t--)
+    {
+        // q++;
+
+        solve();
+    }
+    return 0;
+}
